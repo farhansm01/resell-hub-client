2c86df665527e2f9ea5c1294201ac0d33998f446
Farhan Sadiq <farhansadiq2021@gmail.com>
2026-06-27 12:15:15 +0600

    changed harcoded url to env variables in wishlist api file

--- src/lib/api/wishlist.js
+++ src/lib/api/wishlist.js
@@ -1,9 +1,7 @@
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000";
 
-// src/lib/api/wishlist.js
-
 export async function getWishlist(userId) {
-  const res = await fetch(`http://localhost:5000/api/wishlist/${userId}`);
+  const res = await fetch(`${BASE_URL}/api/wishlist/${userId}`);
 
   if (!res.ok) {
     const errorData = await res.json().catch(() => ({}));
